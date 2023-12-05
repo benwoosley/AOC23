b522627559b1d06a5f3015403c215fbcdd46206f
benwoosley <benjaminwoosley@gmail.com>
2023-12-05 03:06:31 -0500

    Day3 Part 2

--- AdventOfCode/Day3.cs
+++ AdventOfCode/Day3.cs
@@ -1,3 +1,5 @@
+using System.IO.Pipes;
+
 namespace AdventOfCode;
 
 using static Utility;
@@ -6,6 +8,18 @@ public abstract class Day3
 {
     private const string InputFile = "/Users/bdw/RiderProjects/AdventOfCode/AdventOfCode/input/day3_input.txt";
     private const string TestFile = "/Users/bdw/RiderProjects/AdventOfCode/AdventOfCode/input/day3_test.txt";
+
+    private static readonly List<List<int>> Directions = new List<List<int>>
+    {
+        new List<int>() { -1, -1 }, // behind top diagonal
+        new List<int>() { -1, 0 }, // behind
+        new List<int>(){-1, 1}, // behind bottom diagonal
+        new List<int>(){0, 1}, // below 
+        new List<int>(){1, 1}, // front bottom diagonal 
+        new List<int>(){1, 0}, // front 
+        new List<int>(){1, -1}, // front top diagonal
+        new List<int>(){0, -1} // above 
+    };
 
     // directions:
     // front
@@ -23,17 +37,6 @@ public abstract class Day3
     
     public static int Part1()
     {
-        var directions = new List<List<int>>
-        {
-            new List<int>() { -1, -1 }, // behind top diagonal
-            new List<int>() { -1, 0 }, // behind
-            new List<int>(){-1, 1}, // behind bottom diagonal
-            new List<int>(){0, 1}, // below 
-            new List<int>(){1, 1}, // front bottom diagonal 
-            new List<int>(){1, 0}, // front 
-            new List<int>(){1, -1}, // front top diagonal
-            new List<int>(){0, -1} // above 
-       };
        var total = 0;
        var lines = GetLines(InputFile);
        var currNum = "";
@@ -46,7 +49,7 @@ public abstract class Day3
                if (char.IsDigit(lines[i][j]))
                {
                    currNum += lines[i][j];
-                   if (directions.Any(direction => lines[i + direction[0]][j + direction[1]] != '.' &&
+                   if (Directions.Any(direction => lines[i + direction[0]][j + direction[1]] != '.' &&
                                                    !char.IsDigit(lines[i + direction[0]][j + direction[1]])))
                    {
                        isValid = true;
@@ -66,9 +69,70 @@ public abstract class Day3
        return total;
     }
 
+    private static string GetNumber(IReadOnlyList<string> lines, int iIdx, int jIdx, bool[,] visited)
+    {
+        var res = "";
+        res += lines[iIdx][jIdx];
+        visited[iIdx,jIdx] = true;
+        var left = jIdx - 1;
+        var right = jIdx + 1;
+        if (char.IsDigit(lines[iIdx][left]))
+        {
+            while (jIdx < lines[0].Length && char.IsDigit(lines[iIdx][left]))
+            {
+                res += lines[iIdx][left];
+                visited[iIdx,left] = true;
+                left--;
+            }
+            var strArray = res.ToCharArray();
+            Array.Reverse(strArray);
+            var reversedStr = new string(strArray);
+            res = reversedStr;
+        }
+
+        if (!char.IsDigit(lines[iIdx][right])) return res;
+        while (jIdx < lines[0].Length && char.IsDigit(lines[iIdx][right]))
+        {
+            res += lines[iIdx][right];
+            visited[iIdx,right] = true;
+            right++;
+        }
+        return res;
+    }
+    
     public static int Part2()
     {
         var total = 0;
+        var lines = GetLines(InputFile);
+        
+        var visited = new bool[lines.Count, lines[0].Length];
+        
+        for (var i = 1; i < lines.Count - 1; i++)
+        {
+            for (var j = 1; j < lines[i].Length; j++)
+            {
+                if (lines[i][j] != '*') continue;
+                // found a '*'
+                var countNumbers = 0;
+                var currNums = new List<string>();
+                foreach (var direction in Directions)
+                {
+                    var iIdx = i + direction[0];
+                    var jIdx = j + direction[1];
+                    if (char.IsDigit(lines[iIdx][jIdx]) && !visited[iIdx,jIdx])
+                    {
+                        countNumbers++;
+                        var num = GetNumber(lines, i + direction[0], j + direction[1], visited);
+                        currNums.Add(num);
+                    }
+                }
+
+                if (countNumbers == 2)
+                {
+                    total += int.Parse(currNums[1]) * int.Parse(currNums[0]);
+                }
+            }
+        }
         return total;
     }
 }

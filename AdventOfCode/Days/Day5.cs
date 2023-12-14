443a01514613d311b37615d94a4356bf623b08e4
benwoosley <benjaminwoosley@gmail.com>
2023-12-14 00:58:20 -0500

    Day5 Part 1

--- AdventOfCode/Days/Day5.cs
+++ AdventOfCode/Days/Day5.cs
@@ -1,16 +1,70 @@
+using System.ComponentModel;
 using AdventOfCode.Interfaces;
+using static AdventOfCode.Utilities.InputUtility;
 
 namespace AdventOfCode.Days;
 
-public class Day5: IDay
+internal struct Map()
+{
+    public long DestRangeStart { get; init;  }
+    public long SourceRangeStart { get; init; }
+    public long RangeLength { get; init; }
+}
+
+public class Day5 : IDay
 {
     public string Title { get; set; } = "--- Day 5: If You Give A Seed A Fertilizer ---";
-    private const string InputFile = "";
-    private const string TestFile = "";
-        
+    private const string InputFile = "/Users/bdw/RiderProjects/AdventOfCode/AdventOfCode/Input/day5_input.txt";
+    private const string TestFile = "/Users/bdw/RiderProjects/AdventOfCode/AdventOfCode/Input/day5_test.txt";
+
+    // TODO: only check if the current seed is within the range (i.e. start + lengthOf Ranges) and whatever length of ranges is navigate to dest + thatLength 
+    // thatLength may be startDest - currSeed
+    // This naive map solution TLEs on the Input File
     public object Part1()
     {
-        return -1;
+        var lines = GetLines(InputFile);
+        var seeds = lines[0].Split(':')[1].Split(' ').Where(x => x != "").Select(long.Parse).ToList();
+        var maps = new List<List<Map>>();
+        for (var i = 1; i < lines.Count; i++)
+        {
+            if (!lines[i].Contains(':')) continue;
+            var currList = new List<Map>();
+            i++;
+            while (i < lines.Count && lines[i] != "")
+            {
+                var nums = lines[i].Split(' ').Where(x => x != "").ToList();
+                var currMap = new Map
+                {
+                    DestRangeStart = long.Parse(nums[0]),
+                    SourceRangeStart = long.Parse(nums[1]),
+                    RangeLength = long.Parse(nums[2])
+                };
+                currList.Add(currMap);
+                i++;
+            }
+            maps.Add(currList);
+        }
+
+
+        var res = long.MaxValue;
+        
+        foreach (var seed in seeds)
+        {
+            var currValue = seed;
+            foreach (var mapList in maps)
+            {
+                foreach (var map in mapList)
+                {
+                    if (currValue < map.SourceRangeStart || currValue > map.SourceRangeStart + map.RangeLength)
+                        continue;
+                    currValue = map.DestRangeStart + (currValue - map.SourceRangeStart);
+                    break;
+                }
+            }
+            res = long.Min(res, currValue);
+        }
+
+        return res;
     }
 
     public object Part2()
--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -5,12 +5,13 @@ using AdventOfCode.Interfaces;
 
 var days = new List<IDay>
 {
-    new Day1(),
-    new Day2(),
-    new Day3(),
-    new Day4(),
+    // new Day1(),
+    // new Day2(),
+    // new Day3(),
+    // new Day4(),
     new Day5(),
-    new Day6(),
+    // new Day6(),
+    // new Day7(),
 };
 
 foreach (var day in days)
